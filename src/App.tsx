0a1200627dfc8f410d460647bf9833943c0d1185
Vaibhavi <135148044+Vaibhavi028@users.noreply.github.com>
2024-03-09 19:35:27 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import SignupMain from "./components/SignupMain";
 import Signin from "./components/Signin";
+import Woman from "./components/Woman";
+import WomanProfile from "./components/WomanProfile";
+import WomanUserProfile from "./components/WomanUserProfile";
+import WomenProfiles from "./components/WomenProfiles";
+import SignUpWomen from "./components/SignUpWomen";
+import Navbar from "./components/Navbar";
 const App = () => {
   return (
     <div>
-  {/*<SignupMain /> */}
+      <Navbar/>
       <SignupMain />
     </div>
   );
