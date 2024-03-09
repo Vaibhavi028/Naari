9f0697b8387654ae5c3cc95c0ee319f402b7848f
Vaibhavi <135148044+Vaibhavi028@users.noreply.github.com>
2024-03-09 11:40:25 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import './App.css';
 import Signup from './Signup';
+import Signin from './signin';
 import './style.css';
 
 function App() {
-  const [currentPage, setCurrentPage] = useState('Signup');
+  const [currentPage, setCurrentPage] = useState('Signinuser');
 
   const handleLandingpage = () => {
     setCurrentPage('landingpage');
@@ -14,12 +15,16 @@ function App() {
     setCurrentPage('Signup');
   };
 
+  const handleViewSignin = () => {
+    setCurrentPage('Signin');
+  };
+
 
   return (
     <div>
       {currentPage === 'landingpage' && <landingpage onViewSignup={handleViewSignup} />}
       {currentPage === 'Signup' && <Signup onSignupSuccess={handleLandingpage} />}
-  
+      {currentPage === 'Signin' && <Signin onSignup={handleViewSignin} />}
     </div>
   );
 }
