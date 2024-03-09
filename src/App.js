6128c75c863eb8bb042c2b07d16b4c04bd171f30
Ishika Agarwal <135102149+ishAg20@users.noreply.github.com>
2024-03-09 11:55:36 +0530

    Update App.js
    Import 2 different Sign Up pages for rural women(creators, artisans, producers) and for organisations/individuals looking to invest/donate.

--- src/App.js
+++ src/App.js
@@ -1,33 +1,48 @@
-import React, { useState } from 'react';
-import './App.css';
-import Signup from './Signup';
-import Signin from './signin';
-import './style.css';
+// import React, { useState } from 'react';
+// import './App.css';
+// import Signup from './Signup';
+// import Signin from './signin';
+// import './style.css';
 
-function App() {
-  const [currentPage, setCurrentPage] = useState('Signinuser');
+// function App() {
+//   const [currentPage, setCurrentPage] = useState('Signinuser');
 
-  const handleLandingpage = () => {
-    setCurrentPage('landingpage');
-  };
+//   const handleLandingpage = () => {
+//     setCurrentPage('landingpage');
+//   };
   
-  const handleViewSignup = () => {
-    setCurrentPage('Signup');
-  };
+//   const handleViewSignup = () => {
+//     setCurrentPage('Signup');
+//   };
 
-  const handleViewSignin = () => {
-    setCurrentPage('Signin');
-  };
+//   const handleViewSignin = () => {
+//     setCurrentPage('Signin');
+//   };
 
 
+//   return (
+//     <div>
+//       {currentPage === 'landingpage' && <landingpage onViewSignup={handleViewSignup} />}
+//       {currentPage === 'Signup' && <Signup onSignupSuccess={handleLandingpage} />}
+//       {currentPage === 'Signin' && <Signin onSignup={handleViewSignin} />}
+//     </div>
+//   );
+// }
+
+
+// export default App;
+
+import React from "react";
+import SignUpWomen from "./components/SignUpWomen";
+import SignupPage from "./components/SignupPage";
+const App = () => {
   return (
     <div>
-      {currentPage === 'landingpage' && <landingpage onViewSignup={handleViewSignup} />}
-      {currentPage === 'Signup' && <Signup onSignupSuccess={handleLandingpage} />}
-      {currentPage === 'Signin' && <Signin onSignup={handleViewSignin} />}
+      <SignUpWomen />
+      <SignupPage />
     </div>
   );
-}
-
+};
 
 export default App;
+
