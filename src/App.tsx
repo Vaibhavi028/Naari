12403ff426103fd2d9d59cc7f01d0cbd9ae77ffc
Ishika Agarwal <135102149+ishAg20@users.noreply.github.com>
2024-03-09 12:16:18 +0530

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,3 @@
-// import React, { useState } from 'react';
-// import './App.css';
-// import Signup from './Signup';
-// import Signin from './signin';
-// import './style.css';
-
-// function App() {
-//   const [currentPage, setCurrentPage] = useState('Signinuser');
-
-//   const handleLandingpage = () => {
-//     setCurrentPage('landingpage');
-//   };
-  
-//   const handleViewSignup = () => {
-//     setCurrentPage('Signup');
-//   };
-
-//   const handleViewSignin = () => {
-//     setCurrentPage('Signin');
-//   };
-
-
-//   return (
-//     <div>
-//       {currentPage === 'landingpage' && <landingpage onViewSignup={handleViewSignup} />}
-//       {currentPage === 'Signup' && <Signup onSignupSuccess={handleLandingpage} />}
-//       {currentPage === 'Signin' && <Signin onSignup={handleViewSignin} />}
-//     </div>
-//   );
-// }
-
-
-// export default App;
-
 import React from "react";
 import SignUpWomen from "./components/SignUpWomen";
 import SignupPage from "./components/SignupPage";
@@ -45,4 +11,3 @@ const App = () => {
 };
 
 export default App;
-
