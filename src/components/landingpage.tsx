4801dd9375ee569e23c24aaffab50049f71312a9
Vaibhavi <135148044+Vaibhavi028@users.noreply.github.com>
2024-03-10 12:01:19 +0530

    updated

--- src/components/landingpage.tsx
+++ src/components/landingpage.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./landingpg.css";
 import Image from "./img/img1.png";
+import layout from "./img/layout.svg";
 
 const Landingpg: React.FC = () => {
   const [animationStarted, setAnimationStarted] = useState(false);
@@ -12,13 +13,18 @@ const Landingpg: React.FC = () => {
     <div className="container">
       <div className="p1">
         <img src={Image} alt="" />
-        <div className="p2">
+        <div className="p1head">
           <div className="p1text">Helping build LIVES</div>
           <div className="button-container">
         <button className="signin-button">Sign In</button>
         <button className="signup-button">Sign Up</button>
       </div>
+      
         </div>
+        
+      </div>
+      <div className="p1">
+      <img src={layout} className="Layoutimg" alt="" />
       </div>
       
     </div>
